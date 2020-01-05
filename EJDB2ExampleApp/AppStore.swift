0ce04cd5466bf94798cc35b37d97d73f1e09834c
Anton Adamansky <adamansky@gmail.com>
2020-01-06 02:43:58 +0700

    * Code reformat

--- EJDB2ExampleApp/AppStore.swift
+++ EJDB2ExampleApp/AppStore.swift
@@ -11,34 +11,34 @@ import Combine
 import EJDB2
 
 final class AppStore: ObservableObject {
-  
+
   init() throws {
     db = try EJDB2Builder("todolist.db").open()
     try db.ensureStringIndex("tasks", "/id")
     try db.ensureIntIndex("tasks", "/ts")
     try loadAll()
   }
-  
+
   deinit {
     try? db.close()
   }
-  
+
   var db: EJDB2DB;
-  
+
   @Published private(set) var tasks: [Task] = []
-  
+
   func loadAll() throws {
     tasks = try db.createQuery("@tasks/* | desc /ts").list().map {
       try Task.fromJBDOC(self, $0)
     }
   }
-  
+
   func delete(_ task: Task) throws {
     tasks.removeAll(where: {
-      return $0.id == task.id
+      $0.id == task.id
     })
     try db.createQuery("@tasks/[id = :?] | count")
-      .setString(0, "\(task.id)")
-      .executeScalarInt()
+            .setString(0, "\(task.id)")
+            .executeCount()
   }
 }
--- EJDB2ExampleApp/Task.swift
+++ EJDB2ExampleApp/Task.swift
@@ -10,23 +10,23 @@ import SwiftUI
 import EJDB2
 
 struct Task: Equatable, Hashable, Identifiable {
-  
-  static func == (lhs: Task, rhs: Task) -> Bool {
+
+  static func ==(lhs: Task, rhs: Task) -> Bool {
     return lhs.id == rhs.id
   }
-  
+
   static func create(_ store: AppStore, _ title: String) throws -> Task {
     let task = Task(store: store, id: UUID(), title: title, done: false, ts: UInt64(Date().timeIntervalSince1970 * 1000))
     try store.db.put("tasks", task.toJsonMap())
     return task
   }
-  
+
   static func fromJBDOC(_ store: AppStore, _ doc: JBDOC) throws -> Task {
     let id = try UUID(uuidString: doc.at("/id")!)
-    let tsn: NSNumber  = try doc.at("/ts")!
+    let tsn: NSNumber = try doc.at("/ts")!
     return try Task(store: store, id: id!, title: doc.at("/title") ?? "", done: doc.at("/done") ?? false, ts: tsn.uint64Value)
   }
-  
+
   private init(store: AppStore, id: UUID, title: String, done: Bool, ts: UInt64) {
     self.store = store
     self.id = id
@@ -34,36 +34,36 @@ struct Task: Equatable, Hashable, Identifiable {
     self.done = done
     self.ts = ts
   }
-  
+
   func hash(into hasher: inout Hasher) {
     hasher.combine(id)
   }
-  
+
   mutating func patch(_ patch: Any) throws {
     try store.db.createQuery("@tasks/[id = :?] | apply :? | count")
-      .setString(0, "\(id)")
-      .setJson(1, patch)
-      .executeScalarInt()
+            .setString(0, "\(id)")
+            .setJson(1, patch)
+            .executeCount()
   }
-  
+
   mutating func toggleDone() throws {
     done.toggle()
     try patch(["done": done])
   }
-  
+
   mutating func updateTitle(_ title: String) throws {
     self.title = title
     try patch(title)
   }
-  
+
   func delete() throws {
     try store.delete(self)
   }
-  
+
   func toJsonMap() -> Any {
     return ["id": "\(id)", "ts": ts, "title": title, "done": done]
   }
-  
+
   let store: AppStore
   let id: UUID
   let ts: UInt64
